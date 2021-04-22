andreas-schwab/qemu

commit 77280d33bc9cfdbfb5b5d462259d644f5aefe9b3

s390x: fix build for --without-default-devices
s390-pci-vfio.c calls into the vfio code, so we need it to be
built conditionally on vfio (which implies CONFIG_LINUX).

Fixes: cd7498d07fbb ("s390x/pci: Add routine to get the vfio dma available count")
Reported-by: Philippe Mathieu-Daudé <philmd@redhat.com>
Tested-by: Philippe Mathieu-Daudé <philmd@redhat.com>
Reviewed-by: Philippe Mathieu-Daudé <philmd@redhat.com>
Reviewed-by: Matthew Rosato <mjrosato@linux.ibm.com>
Message-Id: <20201103123237.718242-1-cohuck@redhat.com>
Acked-by: Greg Kurz <groug@kaod.org>
Tested-by: Greg Kurz <groug@kaod.org>
Signed-off-by: Cornelia Huck <cohuck@redhat.com>

include/hw/s390x/s390-pci-vfio.h
@@ -13,8 +13,9 @@
 #define HW_S390_PCI_VFIO_H
 
 #include "hw/s390x/s390-pci-bus.h"
+#include CONFIG_DEVICES
 
-#ifdef CONFIG_LINUX
+#ifdef CONFIG_VFIO
 bool s390_pci_update_dma_avail(int fd, unsigned int *avail);
 S390PCIDMACount *s390_pci_start_dma_count(S390pciState *s,
                                           S390PCIBusDevice *pbdev);

hw/s390x/meson.build
@@ -27,7 +27,7 @@ s390x_ss.add(when: 'CONFIG_KVM', if_true: files(
 ))
 s390x_ss.add(when: 'CONFIG_S390_CCW_VIRTIO', if_true: files('s390-virtio-ccw.c'))
 s390x_ss.add(when: 'CONFIG_TERMINAL3270', if_true: files('3270-ccw.c'))
-s390x_ss.add(when: 'CONFIG_LINUX', if_true: files('s390-pci-vfio.c'))
+s390x_ss.add(when: 'CONFIG_VFIO', if_true: files('s390-pci-vfio.c'))
 
 virtio_ss = ss.source_set()
 virtio_ss.add(files('virtio-ccw.c'))

commit fec4a7177d8e2d2b4f4fd30b355a38cbc4800cb9

revert s390x: fix build for --without-default-devices
Include-If: %if 0%{?suse_version} == 1315
References: bsc#1179725

revert 0157-s390x-fix-build-for-without-default.patch

This broke QEMU builds where the --without-default-devices option is
specified during configuration.
Backport patch 0157-s390x-fix-build-for-without-default.patch
subsequently changes this dependency from CONFIG_LINUX to CONFIG_VFIO,
which made sense upstream however it looks to me like CONFIG_VFIO does not
exist at this level of QEMU, meaning it's always OFF and
this is why we never build in s390-pci-vfio.o

Fixes: 77280d33bc ("s390x: fix build for --without-default-devices
(v5.2.0-rc1)")

Signed-off-by: Cho, Yu-Chen <acho@suse.com>

hw/s390x/Makefile.objs
@@ -8,7 +8,7 @@ obj-y += css.o
 obj-y += s390-virtio-ccw.o
 obj-y += 3270-ccw.o
 obj-y += virtio-ccw.o
-obj-$(CONFIG_VFIO) += s390-pci-vfio.o
+obj-$(CONFIG_LINUX) += s390-pci-vfio.o
 obj-$(CONFIG_VIRTIO_SERIAL) += virtio-ccw-serial.o
 obj-$(CONFIG_VIRTIO_BALLOON) += virtio-ccw-balloon.o
 obj-$(CONFIG_VIRTIO_SCSI) += virtio-ccw-scsi.o

include/hw/s390x/s390-pci-vfio.h
@@ -13,9 +13,8 @@
 #define HW_S390_PCI_VFIO_H
 
 #include "hw/s390x/s390-pci-bus.h"
-#include "config-devices.h"
 
-#ifdef CONFIG_VFIO
+#ifdef CONFIG_LINUX
 bool s390_pci_update_dma_avail(int fd, unsigned int *avail);
 S390PCIDMACount *s390_pci_start_dma_count(S390pciState *s,
                                           S390PCIBusDevice *pbdev);